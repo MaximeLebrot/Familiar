812c9fb025b6a161a85cbc3024e4d6451ac83539
hannahellberg <hannahellberg@outlook.com>
2021-04-27 11:02:12 +0200

    Spider healthbar

---
+++ Familiar/Assets/FaceCamera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    public Camera cameraToLookAt;
+
+    void Start()
+    {
+        //transform.Rotate( 180,0,0 );
+    }
+
+    void Update()
+    {
+        Vector3 v = cameraToLookAt.transform.position - transform.position;
+        v.x = v.z = 0.0f;
+        transform.LookAt(cameraToLookAt.transform.position - v);
+        transform.Rotate(0, 180, 0);
+    }
+}
--- Familiar/Assets/Scripts/State/Enemy2/Enemy2.cs
+++ Familiar/Assets/Scripts/State/Enemy2/Enemy2.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.AI; //Navmesh https://docs.unity3d.com/Manual/nav-HowTos.html
+using UnityEngine.UI;
 
 public class Enemy2 : MonoBehaviour, IZappable
 {
@@ -19,12 +20,16 @@ public class Enemy2 : MonoBehaviour, IZappable
 
     private StateMachine stateMachine;
 
+    public Slider slider;
+
     protected void Awake()
     {
         idlePosition = transform;
         player = GameObject.FindGameObjectWithTag("Player");
         navAgent = GetComponent<NavMeshAgent>();
         stateMachine = new StateMachine(this, states);
+
+        slider.value = 1.0f;
     }
 
     private void Update()
@@ -49,6 +54,7 @@ public class Enemy2 : MonoBehaviour, IZappable
     public void OnZap()
     {
         health--;
+        slider.value -= 0.25f;
         //Destroy(gameObject);
     }
 }
