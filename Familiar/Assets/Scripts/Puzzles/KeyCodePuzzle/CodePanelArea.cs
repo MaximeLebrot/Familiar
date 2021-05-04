2bca2e85588ddfafb2e75f0da7a960a548bcde15
AstridPa <asan2611@student.su.se>
2021-05-04 10:34:05 +0200

    Added help text for code panel lv2

--- Familiar/Assets/Scripts/Puzzles/KeyCodePuzzle/CodePanelArea.cs
+++ Familiar/Assets/Scripts/Puzzles/KeyCodePuzzle/CodePanelArea.cs
@@ -1,20 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CodePanelArea : MonoBehaviour
 {
     private AbilitySystem.Player player;
 
+    // A text telling the player which button to press to open the code panel UI
+    public Text helpText;
+    public string instructions;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<AbilitySystem.Player>();
+        helpText.text = "";
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             player.isInCodePanelArea = true;
+
+            if (player.canSeeCodePanel)
+            {
+                helpText.text = instructions;
+            }
         }
     }
     private void OnTriggerExit(Collider other)
@@ -22,6 +33,7 @@ public class CodePanelArea : MonoBehaviour
         if (other.tag == "Player")
         {
             player.isInCodePanelArea = false;
+            helpText.text = "";
         }
     }
 }
