79b5a709481b2986f432919d654bfa1ce6823afa
AstridPa <asan2611@student.su.se>
2021-05-05 12:01:35 +0200

    More dialogue in level 1!

--- Familiar/Assets/Scripts/HelpTextTrigger.cs
+++ Familiar/Assets/Scripts/HelpTextTrigger.cs
@@ -5,12 +5,20 @@ using UnityEngine.UI;
 
 public class HelpTextTrigger : MonoBehaviour
 {
-    // A class that sets an UI text. Can be destroyed, if needed.
+    // A class that sets an UI text via triggers who use this script.
+    // The trigger can be deactivated via bool.
+    // Theres also a bool for keeping the text permanent and not resetting it.
+    // Scripts been updated to also work for the key!
     public Text helpText;
     public string instructions;
     public float waitTime;
     public bool deactiveTrigger;
     public bool permanentText;
+
+    // A little safety bool so the player doesn't trigger stuff it shouldn't without the key.
+    public bool needKey;
+
+    public float typeSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -20,19 +28,36 @@ public class HelpTextTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && !needKey)
         {
+            //StartCoroutine(TypeText());
             helpText.text = instructions;
             if (deactiveTrigger)
             {
                 StartCoroutine(WaitAndDestroy());
             }
         }
+        else if (other.tag == "Key" && needKey)
+        {
+            //StartCoroutine(TypeText());
+            helpText.text = instructions;
+        }
+    }
+
+    // Added so the letters type out one after another. Not in use!
+    private IEnumerator TypeText()
+    {
+        helpText.text = "";
+        foreach (char letter in instructions.ToCharArray())
+        {
+            helpText.text += letter;
+            yield return new WaitForSeconds(typeSpeed);
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && !needKey)
         {
             if (helpText.text == instructions && !permanentText)
             {
