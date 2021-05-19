c74cdaac0263f015240da71e540bf37d09e178d9
Jekadu <simon@canback.net>
2021-05-20 01:52:19 +0200

    Revert "Revert "level 1 out of bounds""
    This reverts commit e930ed834284d165afd370aa6e05db3738bee818.

---
+++ Familiar/Assets/MoveableBase.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Unity;
+
+public abstract class MoveableBase : MonoBehaviour, IMoveable
+{
+    public abstract bool IsCarried
+    {
+        get;
+    }
+
+    public abstract GameObject Carrier
+    {
+        get;
+        set;
+    }
+
+    public abstract Vector3 ResetPoint
+    {
+        get;
+        set;
+    }
+
+    private void Awake()
+    {
+        
+    }
+}
